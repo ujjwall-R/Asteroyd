d0178fff70243b0021f91823c3a02692dfe9f4dd
Ujjwal R <ujjwal.raj.5798@gmail.com>
2023-09-16 00:48:00 +0530

    test setup : ready for TDD

--- test/hello-world-service.unit.test.ts
+++ test/hello-world-service.unit.test.ts
@@ -1,9 +1,13 @@
 import { expect } from 'chai';
 import { mockFunction } from '../src/mock';
+var assert = require('assert');
 
 describe('mockFunction', () => {
   it('should return true', () => {
     const result: boolean = mockFunction();
     expect(result).to.be.true;
   });
+  it('2 + 2 should return 4', function() {
+    assert.equal(2+2, 4);
+});
 });
